45249522e855bfd3d72f1f5e2c55f7817fa50ecf
Greg4016 <gerike.kiss85@gmail.com>
2024-02-28 12:27:48 +0100

    Added header area

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
 import Card from './Card'
 import smachineimg from './imgs/state-machine.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-slate-500 h-screen flex justify-center items-center flex-wrap'>
-        <Card title={"State Machine [WIP]"} sub={'HTML, CSS, JS'} href={"https://greg4016.github.io/state-machine/"} img={smachineimg}></Card>
-        <Card name={"proj1"}></Card>
-        <Card name={"proj1"}></Card>
-        <Card name={"proj1"}></Card>
+      <div id='header' className='bg-slate-700 h-screen'></div>
+      <div id='cards' className='bg-slate-500 h-fit flex justify-center items-center flex-wrap'>
+        <Card id='card1' title={"State Machine [WIP]"} sub={'HTML, CSS, JS'} href={"https://greg4016.github.io/state-machine/"} img={smachineimg}></Card>
+        <Card id='card2' name={"proj1"}></Card>
+        <Card id='card3' name={"proj1"}></Card>
+        <Card id='card4' name={"proj1"}></Card>
       </div>
     </>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 
-function Card({title, sub, href, img}) {
+function Card({title, sub, href, img, id}) {
 
     const [hovering, setHovering] = useState(false)
 
 
 
     return (
-        <div className="w-[16rem] h-[24rem] relative bg-slate-300 m-4" onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
+        <div id={id} className="w-[16rem] h-[24rem] relative bg-slate-300 m-4" onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)}>
             <a href={href} target="blank">
                 <img src={img} className="h-full object-cover"></img>
-                <div className={`${hovering ? 'h-full' : 'h-[10%]'} transition-all w-full bg-opacity-80 gap-4 text-white bg-black absolute bottom-0 flex flex-col justify-center items-center`}>
+                <div className={`${hovering ? 'h-full' : 'h-[10%]'} card`}>
                     <span className='text-lg'>{title}</span>
                     {hovering && (
                         <span className='text-sm'>{sub}</span>
